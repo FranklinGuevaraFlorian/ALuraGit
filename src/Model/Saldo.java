5951dd4573873d2e6060b953946e06f966a9df28
Franklin Guevara <franklingueflo@hotmail.com>
2024-02-12 14:24:03 -0500

    agregando char para que haya compatibilidad

--- src/Model/Saldo.java
+++ src/Model/Saldo.java
@@ -14,7 +14,7 @@ public class Saldo {
     public Byte devolverEdad(byte edad){return null;}
     private Integer a;
     private Boolean b;
-
+    private char c;
     @Override
     public String toString() {
         return "Saldo total disponible:" + getDineroDisponible();
